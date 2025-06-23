5b044b0e24032d73e9608bec4070743ab294cb5a
rafuwong <rafuyiwong@gmail.com>
2025-06-23 15:38:25 -0500

    Working Calculator and Styling
    Added operator function and basic styling.
    Updated HTML structure to support CSS styling updates.

--- calc-functions.js
+++ calc-functions.js
@@ -45,53 +45,63 @@ let displayVal = ""; //Initializing displayVal to an empty string
 
 num7.addEventListener("click", () => {
     console.log("7");
-    displayVal = "7";
+    displayVal += "7";
+    updateCalcString("7");
     updateDisplay();
 })
 num8.addEventListener("click", () => {
     console.log("8");
-    displayVal = "8";
+    displayVal += "8";
+    updateCalcString("8");
     updateDisplay();
 
 })
 num9.addEventListener("click", () => {
     console.log("9");
-    displayVal = "9";
+    displayVal += "9";
+    updateCalcString("9");
     updateDisplay();
 })
 num4.addEventListener("click", () => {
     console.log("4");
-    displayVal = "4";
+    displayVal += "4";
+    updateCalcString("4");
     updateDisplay();
 })
 num5.addEventListener("click", () => {
     console.log("5");
-    displayVal = "5";
+    displayVal += "5";
+    updateCalcString("5");
     updateDisplay();
 })
 num6.addEventListener("click", () => {
     console.log("6");
-    displayVal = "6";
+    displayVal += "6";
+    updateCalcString("6");
     updateDisplay();
 })
 num1.addEventListener("click", () => {
     console.log("1");
-    displayVal = "1";
+    displayVal += "1";
+    updateCalcString("1");
     updateDisplay();
 })
 num2.addEventListener("click", () => {
     console.log("2");
-    displayVal = "2";
+    displayVal += "2";
+    updateCalcString("2");
     updateDisplay();
 })
 num3.addEventListener("click", () => {
     console.log("3");
-    displayVal = "3";
+    displayVal += "3";
+    updateCalcString("3");
     updateDisplay();
 })
 num0.addEventListener("click", () => {
     console.log("0");
-    displayVal = "0";
+    displayVal += "0";
+    updateCalcString("0");
     updateDisplay();
 })
 
@@ -101,35 +111,103 @@ const subtractButton = document.querySelector(".opSubt");
 const multiplyButton = document.querySelector(".opMult");
 const divideButton = document.querySelector(".opDiv");
 const clearButton = document.querySelector(".clear");
-
+const equalsButton = document.querySelector(".opEqual");
 addButton.addEventListener("click", () => {
     console.log("Add");
+    updateCalcString("+");
+    displayVal = ""; // Clear displayVal after operation
+    updateDisplay();
 })
 subtractButton.addEventListener("click", () => {
     console.log("Subtract");
+    updateCalcString("-");
+    displayVal = ""; // Clear displayVal after operation
+    updateDisplay();
 })
 multiplyButton.addEventListener("click", () => {
     console.log("Multiply");
+    updateCalcString("*");
+    displayVal = ""; // Clear displayVal after operation
+    updateDisplay();
 })
 divideButton.addEventListener("click", () => {
     console.log("Divide");
+    updateCalcString("/");
+    displayVal = ""; // Clear displayVal after operation
+    updateDisplay();
 })
 clearButton.addEventListener("click", () => {
     console.log("Clear");
     clearDisplay();
+    currentString = "";
+})
+
+equalsButton.addEventListener("click", () => {
+    console.log("Equals");
+    updateCalcString("=");
+    // After equals, we can clear displayVal or keep it for further calculations
+    updateDisplay();
 })
 
 let display = document.querySelector(".display");
 
 function updateDisplay() {
-    if (isNumeric(displayVal)) {
-        display.textContent = displayVal.toString();
-    } else {
-        display.textContent = displayVal.toString() + displayVal.toString().slice(1);
-    } 
+    display.textContent = displayVal.toString();
 }
 
 function clearDisplay() {
     displayVal = "";
     updateDisplay();
+}
+
+currentString = "";
+function updateCalcString(string) {
+    let operatorSigns = ["+", "-", "*", "/"];
+    let hasOperator = operatorSigns.includes(string);
+    // console.log("Has Operator: " + hasOperator);
+    if (hasOperator) {
+        // console.log("Operator DETECTED");
+        currentString += " " + string + " ";
+    } else if (string === "=") {
+        let parts = currentString.split(" ");
+        displayVal = operator(parts);
+        return operator(parts);
+    } else {
+        currentString += string;
+    }
+    console.log("Current Calculation String: " + currentString);
+    }
+
+function operator(lis) {
+    let firstNum = parseFloat(lis[0]);
+    let secondNum = parseFloat(lis[2]);
+    let operator = lis[1];
+
+    if (operator === "+") {
+        let sumNum = add(firstNum, secondNum);
+        currentString = sumNum.toString();
+        updateDisplay();
+        return sumNum;
+    }
+    else if (operator === "-") {
+        let subNum = subtract(firstNum, secondNum);
+        currentString = subNum.toString();
+        updateDisplay();
+        return subNum;
+    }
+    else if (operator === "*") {
+        let multNum = multiply(firstNum, secondNum);
+        currentString = multNum.toString();
+        updateDisplay();
+        return multNum;
+    }
+    else if (operator === "/") {
+        let divNum = divide(firstNum, secondNum);
+        currentString = divNum.toString();
+        updateDisplay();
+        return divNum;
+    }
+    else {
+        throw new Error("Invalid operator.");
+    }
 }
